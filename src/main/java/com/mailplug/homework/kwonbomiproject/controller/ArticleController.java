efcdb8741f1e501ef3814d4e79740bbd5ced9965
Bomi-kwon <bbomicoding@gmail.com>
2023-08-02 11:17:44 +0900

    테스트 업로드

--- src/main/java/com/mailplug/homework/kwonbomiproject/controller/ArticleController.java
+++ src/main/java/com/mailplug/homework/kwonbomiproject/controller/ArticleController.java
@@ -1,8 +1,16 @@
 package com.mailplug.homework.kwonbomiproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ArticleController {
-	//
+	// 첫 연결 테스트
+	
+	@RequestMapping("")
+	public String showList() {
+		
+		
+		return "";
+	}
 }
